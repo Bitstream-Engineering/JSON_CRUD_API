9a0c2fdfe19dbcea5e25cbd0910ccf808406e2bd
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-13 17:10:34 +0300

    Post struct field added

--- models/postModel.go
+++ models/postModel.go
@@ -1,1 +1,11 @@
+// GORM simplifies database interactions by mapping Go structs to database tables.
+
 package models
+
+import "gorm.io/gorm"
+
+type Post struct {
+	gorm.Model
+	Title string
+	Body  string
+}
