4fa024181d744aaac7e8ea5ea111eaca92d96309
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-13 15:27:06 +0300

    More code

--- initializers/database.go
+++ initializers/database.go
@@ -3,6 +3,7 @@
 package initializers
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -14,6 +15,7 @@ func ConnectToDb() {
 	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
+	fmt.Println(DB)
 	if err != nil {
 		log.Fatal("Error opening the database.")
 	}
