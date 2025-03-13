70c184088e4abffcd7bc9cd9c714bda8553e2e35
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-13 19:05:11 +0300

    More code in loadEnvVariables.go

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,11 +3,21 @@ package initializers
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 // LoadEnvVariables loads the .env file
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	//err := godotenv.Load(".env")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	environmentPath := filepath.Join(dir, ".env")
+
+	godotenv.Load(environmentPath)
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
