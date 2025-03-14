ce9a6c8735cf013b19e4263a57c5a6d1b4cadff9
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-14 12:43:10 +0300

    More code

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -1,9 +1,22 @@
 package controllers
 
-import "C"
-import "github.com/gin-gonic/gin"
+import (
+	"JSON_CRUD_API/initializers"
+	"JSON_CRUD_API/models"
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func PostsCreate(c *gin.Context) {
+
+	post := models.Post{Title: "Title", Body: "Post Body"}
+
+	result := initializers.DB.Create(&post) // pass pointer of data to Create
+
+	if result.Error != nil {
+		log.Fatal("Error in initializers.DB.Create(&post) ")
+	}
+
 	c.JSON(200, map[string]any{
 		"message": "pong",
 	})
