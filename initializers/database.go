874fd0cfd6f466142b690c4b6ba8a2c69a3e339c
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-14 09:41:28 +0300

    More code

--- initializers/database.go
+++ initializers/database.go
@@ -3,7 +3,6 @@
 package initializers
 
 import (
-	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -15,15 +14,13 @@ var DB *gorm.DB
 func ConnectToDb() {
 	//dsn := os.Getenv("DB_URL")
 
-	fmt.Println("Above dsn := ")
 	dsn := "host=localhost user=postgres dbname=json_crud_api port=5432 sslmode=disable"
-	fmt.Println("Middle of dsn & DB,err")
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Error opening the database.")
 	}
-	fmt.Println("Printing DB", DB)
 
 }
 
