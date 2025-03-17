218359e797218e0bdf19a0a4c84da8a771ded523
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-17 10:50:01 +0300

    More code

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ func main() {
 	// GIN boilerplate code
 	//gin.SetMode(gin.ReleaseMode) //Set for production use
 	r := gin.Default()
-	r.POST("/posts", controllers.PostsCreate)
+	r.POST("/posts", controllers.PostsCreate)    // POST is a safe method because it doesn't modify the state of the server
+	r.PUT("/posts/:id", controllers.PostsUpdate) // PUT updates or replaces existing data on server
 	r.GET("/posts", controllers.PostsIndex)
 	r.GET("/posts/:id", controllers.PostsShow)
 
